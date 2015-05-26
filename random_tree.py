027f93ec3bbdf2b30ead80602103e27131aae456
Ivan Alejandro <ivanalejandro0@gmail.com>
2015-05-26 01:15:43 -0300

    Some improvements over the existing code.

--- random_tree.py
+++ random_tree.py
@@ -9,25 +9,80 @@ filename and then encrypt it.
 """
 
 import base64
+import json
 import os
 
 
 class RandomTree(object):
+    BASE_PATH = './random.tree/'
+
     def __init__(self):
         self._map = {}
         # TODO: decrypt, load
+        self._load_map()
 
     def _get_random_name(self):
-        r = os.urandom(32)
-        return base64.urlsafe_b64encode(r)
+        # r = os.urandom(32)
+        # return base64.urlsafe_b64encode(r)
+        # shorter, easier to read on tests
+        r = os.urandom(16)
+        return base64.b16encode(r)
 
-    def _save(self, name, value):
-        self._map[name] = value
-        # TODO: persist, encrypt
+    def _save_map(self):
+        with open(self.BASE_PATH + 'file_names.json', 'w') as f:
+            f.write(json.dumps(self._map))
+
+    def _load_map(self):
+        jdata = None
+        try:
+            with open(self.BASE_PATH + 'file_names.json', 'r') as f:
+                jdata = json.load(f)
+        except IOError:
+            pass
+
+        if jdata is not None:
+            self._map = jdata
 
     def add_node(self, name):
-        rnd_name = self._get_random_name()
-        self._save(name, rnd_name)
+        # partial names?
+        # some/ -> randomA
+        # some/thing/ -> randomA/randomB
+        # some/thing/test -> randomA/randomB/randomC
+        # if some of the random paths are already created we should use them
+        # import ipdb; ipdb.set_trace()
+        if self._map.get(name):
+            raise Exception("The node already exist.")
+
+        print '-'*10
+        print "name:", name
+        rnd_path = ''
+        real_path = ''
+        for d in name.split('/'):
+            if real_path:
+                real_path += '/'
+            real_path += d
+
+            print 'd:', d
+            if rnd_path:
+                rnd_path += '/'
+            if self._map.get(d):
+                rnd_path += self._map[d]
+            else:
+                rnd_path += self._get_random_name()
+            self._map[real_path] = rnd_path
+
+        if not rnd_path:
+            rnd_path = name
+
+        print "real_path -> rnd_path:", real_path, '->', rnd_path
+        from pprint import pprint
+        print "self._map:"
+        pprint(self._map)
+        self._map[name] = rnd_path
+        # TODO: check if the name we want to save doesn't already exist
+        # TODO: persist, encrypt
+        # self._save_map()
+        print '-'*10
 
     def get_node(self, name):
         return self._map[name]
@@ -38,13 +93,20 @@ class RandomTree(object):
 
 def main():
     t = RandomTree()
+    t.add_node('ivan/test/demo-mail')
+    t.add_node('ivan/asdf')
+    return
 
-    t.add_node('secret-note')
-    t.add_node('ivan/test')
-    t.add_node('ivan/gmail-account')
-    t.add_node('unx/bank-passwords')
-    t.add_node('unx/admin-mail')
-    t.add_node('unx/ISPConfig')
+    try:
+        t.add_node('secret-note')
+        t.add_node('ivan/test-data')
+        t.add_node('ivan/gmail-account')
+        t.add_node('unx/bank-passwords')
+        t.add_node('unx/admin-mail')
+        t.add_node('unx/ISPConfig')
+        t.add_node('ivan/test/demo-mail')
+    except Exception as e:
+        print repr(e)
 
     from pprint import pprint
     pprint(t._map)
--- test-gpg-packets.sh
+++ test-gpg-packets.sh
@@ -1,6 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-exit  # remove this to test, be careful on not to cipher/overwrite your files
+[[ -z $1 ]] && exit
+
+KEY=`pwd`/test_passphrase_file.txt
 cd gpg.test/test-files-tree  # files and directories to encrypt/decrypt as a test
-for f in $(find . -type f); do gpg --no-use-agent -c --batch --yes --passphrase-file `pwd`/../key -o $f $f; done;  # encrypt files
-for f in $(find . -type f); do gpg -q --list-packets --no-use-agent --passphrase-file `pwd`/../key $f 2>&1 | grep --color=never -o 'name=".*"'; done;  # list files names
+
+if [[ $1 == "encrypt" ]]; then
+    for f in $(find . -type f); do
+        gpg --no-use-agent -c --batch --yes --passphrase-file $KEY -o $f $f;
+    done
+fi
+
+if [[ $1 == "list" ]]; then
+    for f in $(find . -type f); do
+        gpg -q --list-packets --no-use-agent --passphrase-file $KEY $f 2>&1 | grep --color=never -o 'name=".*"';
+    done;
+fi
