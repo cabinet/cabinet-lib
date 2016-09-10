68e82663b18271a2203a17303b8ffecad4de47c1
Facundo Victor <facundovt@gmail.com>
2016-09-10 15:23:41 -0300

    Add support for token authentication

--- rpc-server.py
+++ rpc-server.py
@@ -2,14 +2,29 @@
 # -*- coding: utf-8 -*-
 import os
 
-from flask import Flask
+from functools import wraps
+from flask import Flask, session, request
 from flask_jsonrpc import JSONRPC
+from flask_jsonrpc.exceptions import InvalidCredentialsError
 
 from cabinet import Cabinet
 
-app = Flask(__name__)
-jsonrpc = JSONRPC(app, '/api/v1')
+SECURITY_SALT = 'something to be changed in production'
 
+app = Flask(__name__)
+
+
+def authenticate(f, f_check_auth):
+    @wraps(f)
+    def _f(*args, **kwargs):
+        token = request.headers.get('token')
+        if not f_check_auth(token):
+            raise InvalidCredentialsError()
+        return f(*args, **kwargs)
+    return _f
+
+
+jsonrpc = JSONRPC(app, '/api/v1', auth_backend=authenticate)
 
 account_id = 'my-name@my-company.com'
 password = b'asdfasdf'
@@ -21,9 +36,11 @@ cab = Cabinet(account_id, password, temp_config_path)
 cab.open(name, test_vault_path)
 
 
-def check_auth(username, password):
-    # return username == 'myname' and password == 'secret'
-    return True
+def check_auth(token):
+    token = request.headers.get('token')
+    if 'token' in session:
+        return token == session['token']
+    return False
 
 
 @jsonrpc.method('App.get_all')
@@ -46,7 +63,7 @@ def index():
     return u'hello world!'
 
 
-@jsonrpc.method('App.echo(name=str) -> str', validate=True, authenticated=check_auth)
+@jsonrpc.method('App.echo(name=str) -> str', authenticated=check_auth)
 def echo(name='Flask JSON-RPC'):
     return u'Hello {0}'.format(name)
 
@@ -62,6 +79,7 @@ def get_random_open_port():
 
 
 if __name__ == '__main__':
+    app.secret_key = 'something really really secret =D'
     # app.run(host='0.0.0.0', port=5000, debug=True)
     app.run(port=5000, debug=True)  # only localhost, default port
 
