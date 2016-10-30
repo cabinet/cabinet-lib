b8f00a9f01190cebf30d9cfe8cd2cdb789df9206
Facundo Victor <facundovt@gmail.com>
2016-10-30 16:40:17 -0300

    Change method login to open vault

--- rpc-server.py
+++ rpc-server.py
@@ -29,13 +29,14 @@ def check_auth(client_token):
     return token == client_token
 
 
-@jsonrpc.method('App.open(username=str, password=str, vault_path=str) -> str')
-def login(username, password, vault_path):
-    # TODO: For now, the user and password is not being validated for now.
+@jsonrpc.method('App.open_vault(username=str, password=str, vault_path=str)'
+                ' -> str')
+def open_vault(username, password, vault_path):
+    # TODO: Implement username/password/vault validation and opening
     session['username'] = username
     session['password'] = password
     session['vault_path'] = vault_path
-    return 'success'
+    return {'success': True}
 
 
 @jsonrpc.method('App.get_all')
