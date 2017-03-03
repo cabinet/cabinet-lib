64c3e0dac154015fb328c75e9860a2ad3f0f0d14
Facundo Victor <facundovt@gmail.com>
2017-03-03 18:26:04 -0300

    Add docs

--- cabinet.py
+++ cabinet.py
@@ -32,10 +32,22 @@ vault_config = {
 }
 
 
-# @jsonrpc.method('App.open_vault(username=str, password=str,'
-#                 'vault_name=str, vault_path=str) -> str')
 def open_vault(username, password, vault_name, vault_path=None):
-    # TODO: Implement username/password/vault validation and opening
+    """
+    Open the vault identified by the vault_name, vault_path.
+
+    Params:
+    :param username:
+    :ptype: String
+    :param password:
+    :ptype: String
+    :param vault_name: The name of the vault
+    :ptype: String
+    :param vault_path: The location of the vault.
+    :ptype: String
+
+    TODO: Implement username/password/vault validation and opening
+    """
 
     global vault_config
     config_path = vault_config.get('config_path')
@@ -49,28 +61,56 @@ def open_vault(username, password, vault_name, vault_path=None):
     return True  # TODO: Add open check. For this, the vault should verify keys
 
 
-# @jsonrpc.method('App.get_all')
 def get_all():
+    """
+    Get all the items from the vault without the 'content' (i.e: only name and
+    tags).
+
+    :returns: The list of items
+    :ptype: List of Dictionaries.
+    """
     global vault_config
     cab = vault_config.get('cabinet')
     return cab.get_all()
 
 
-# @jsonrpc.method('App.get(name=str)')
 def get_item(name):
+    """
+    Get an item from the vault.
+
+    :param name: The name of the item to recover.
+    :ptype: String
+
+    :returns: The item with the specified name.
+    :ptype: Dictionary
+    """
     global vault_config
     cab = vault_config.get('cabinet')
     return cab.get(name)
 
 
-# @jsonrpc.method('App.add(item=dict)')
 def add_item(item):
+    """
+    Add an item to the vault.
+
+    :param item: The item to be added.
+    :ptype: Dictionary
+    """
     global vault_config
     cab = vault_config.get('cabinet')
-    return cab.add(item)
+    cab.add(item)
 
 
 def tags(value):
+    """
+    Parse the value to recover a list of tags.
+
+    :param value: The value to parse
+    :ptype: String
+
+    :returns: A list of strings composed by spliting the value by ','.
+    :rtype: List
+    """
     try:
         return value.split(',')
     except:
@@ -79,6 +119,15 @@ def tags(value):
 
 
 def tuple(value):
+    """
+    Parse the value to recover a tuple
+
+    :param value: The value to parse
+    :ptype: String
+
+    :returns: A tuple of two values composed by spliting the value by ','.
+    :rtype: Tuple
+    """
     try:
         key, value = value.split(',')
         return key, value
@@ -96,6 +145,7 @@ class CabinetController(CementBaseController):
 
     @expose(hide=True)
     def default(self):
+        """Without any parameter, the command will print the help"""
         app.args.print_help()
 
 
@@ -119,6 +169,7 @@ class ItemController(CementBaseController):
 
     @expose(help='Get an item from the vault.')
     def get(self):
+        """Get an item from the vault"""
         name = self.app.pargs.name
         if name:
             self.app.log.debug('Looking for item with name "{0}"'.format(name))
@@ -134,6 +185,7 @@ class ItemController(CementBaseController):
 
     @expose(help="Add an item to the vault.")
     def add(self):
+        """Add an item to the vault"""
         account_id = 'my-name@my-company.com'
         password = 'asdfasdf'
         vault_name = 'test-vault'
@@ -159,6 +211,8 @@ class ItemController(CementBaseController):
 
 
 class SearchController(CementBaseController):
+    """The command controller for searching within the vault values"""
+
     class Meta:
         label = 'search'
         stacked_on = 'base'
@@ -178,6 +232,7 @@ class SearchController(CementBaseController):
     # TODO: Think where should go the item filter (In the cabinet or here?)
     @expose(help='Get all the items in the vault.')
     def default(self):
+        """Print all the item names (and tags if apply) to stdout."""
         account_id = 'my-name@my-company.com'
         password = 'asdfasdf'
         vault_name = 'test-vault'
@@ -186,13 +241,12 @@ class SearchController(CementBaseController):
             tags = [] if not self.app.pargs.tag else self.app.pargs.tag
         tag_tpl = " tagged with {1}" if self.app.pargs.show_tags else ''
         if open_vault(account_id, password, vault_name):
-            all = get_all()
-            # all = [item for index, item in get_all().items()]
-            # print(all)
+            all = get_all().values()
+            all = [item for item in all if set(tags).issubset(item['tags'])]
             print("The following items was found:")
             for item in all:
-                tags = all[item]['tags']
-                print(('\t-"{0}"' + tag_tpl).format(item, tags))
+                print(('\t-"{0}"' + tag_tpl).format(item['name'],
+                                                    item['tags']))
 
 
 class MyApp(CementApp):
